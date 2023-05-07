52e6a4b618355f66a10c05cc3e6a15c483a66b70
Arun Rathore <arathore@nowoptics.com>
2023-05-08 00:08:46 +0530

    Screen Recording app

--- ScreenRecordingApp/AppDelegate.swift
+++ ScreenRecordingApp/AppDelegate.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import ReplayKit
 
+@available(iOS 13.0, *)
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+  var screenRecorder: RPScreenRecorder!
+  var previewViewController: RPPreviewViewController?
+  var window: UIWindow?
 
+  static var shared: AppDelegate { return UIApplication.shared.delegate as! AppDelegate }
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+
+
     // Override point for customization after application launch.
+   let window = UIWindow(frame: UIScreen.main.bounds)
+    let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as! ViewController
+    let navigationController = UINavigationController(rootViewController: vc)
+    window.rootViewController = navigationController
+    window.makeKeyAndVisible()
+    self.window = window
+    self.screenRecorder = RPScreenRecorder.shared()
     return true
   }
 
@@ -32,5 +48,61 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 
 
+  func recordScreen() {
+    if self.screenRecorder.isAvailable {
+      self.screenRecorder.isMicrophoneEnabled = false
+//      self.screenRecorder.cameraPreviewView
+
+
+      self.screenRecorder.startRecording(handler: { [weak self] (error) in
+        if let error = error {
+          print("Recording failed to start: \(error.localizedDescription)")
+        } else {
+          print("Recording started.")
+        }
+      })
+
+
+
+
+//      self.screenRecorder.startRecording { error in
+//
+//      }
+    }
+  }
+
+  func stopRecording() {
+   self.screenRecorder.stopRecording { [self] (previewViewController, error) in
+     if let error = error {
+       print("Recording failed to stop: \(error.localizedDescription)")
+     } else if let previewViewController = previewViewController {
+       self.previewViewController = previewViewController
+       previewViewController.previewControllerDelegate = self
+       previewViewController.modalPresentationStyle = .fullScreen
+
+
+       if var topController = UIApplication.shared.keyWindow?.rootViewController {
+           while let presentedViewController = topController.presentedViewController {
+               topController = presentedViewController
+           }
+         topController.present(previewViewController, animated: true, completion: nil)
+       }
+     }
+   }
+ }
+
+
 }
 
+@available(iOS 13.0, *)
+extension AppDelegate: RPPreviewViewControllerDelegate {
+  func previewControllerDidFinish(_ previewController: RPPreviewViewController) {
+
+  }
+
+  func previewController(_ previewController: RPPreviewViewController, didFinishWithActivityTypes activityTypes: Set<String>) {
+    if activityTypes.contains("com.apple.UIKit.activity.SaveToCameraRoll") {
+      print("Recording saved to camera roll.")
+    }
+  }
+}
--- ScreenRecordingApp/ViewController.swift
+++ ScreenRecordingApp/ViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
+import ReplayKit
 
 class ViewController: UIViewController {
+  
+  var screenRecorder: RPScreenRecorder?
+  var previewVC: RPPreviewViewController?
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    screenRecorder = RPScreenRecorder.shared()
   }
 
+
+  @IBAction func recordScreen(_ sender: UIButton) {
+    startRecordingWithSender(sender: sender)
+  }
+
+  @IBAction func stopRecording(_ sender: UIButton) {
+    stopRecordingWithSender(sender: sender)
+    //    AppDelegate.shared.stopRecording()
+  }
+
+
+
+
+  private func startRecordingWithSender(sender: UIButton?) {
+    screenRecorder?.isMicrophoneEnabled = true
+    screenRecorder?.startRecording(handler: {[weak self] (error) in
+      guard error == nil else {
+        self?.showErrorAlertWithMesage(message: "Sorry!! Your recording couldn't be started. Please try again.")
+        return
+      }
+    })
+
+
+  }
+
+
+  private func stopRecordingWithSender(sender: UIButton?) {
+
+    screenRecorder?.stopRecording(handler: {[weak self] (previewViewController, error) in
+      DispatchQueue.main.async {
+        guard error == nil else {
+          self?.showErrorAlertWithMesage(message: "Sorry!! Your recording couldn't be stopped. Please try again.")
+          return
+        }
+
+        previewViewController?.previewControllerDelegate = self
+        self?.previewVC = previewViewController
+        if let previewVC = self?.previewVC {
+          self?.present(previewVC, animated: true, completion: nil)
+
+        }
+      }
+    })
+  }
+
+  private func showErrorAlertWithMesage(message: String) {
+
+    let alertController = UIAlertController(title: NSLocalizedString("Error", comment: "Camera Capture"), message:NSLocalizedString("This app doesn't have permission to use the camera. Please change the privacy settings", comment: "Camera Capture"), preferredStyle: UIAlertController.Style.alert)
+
+    let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+      alertController.addAction(okAction)
+      present(alertController, animated: true, completion: nil)
+  }
+
+}
+
+extension ViewController : RPPreviewViewControllerDelegate {
+
+  func previewController(_ previewController: RPPreviewViewController, didFinishWithActivityTypes activityTypes: Set<String>) {
+
+    previewController.dismiss(animated: true) {
+
+//      self.window?.isHidden = false
+
+    }
+  }
 
 }
 
